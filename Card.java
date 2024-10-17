1dee32149eb793789c36e3d6a18ef1338e9cc097
Rikupp17 <marc.stiehm@dci-student.org>
2024-10-17 13:50:54 +0200

    By Marc

--- Card.java
+++ Card.java
@@ -1,2 +1,31 @@
 public class Card {
+    private String symbol;
+    private boolean isFaceUp;
+    private boolean isMatched;
+
+    public Card(String symbol) {
+        this.symbol = symbol;
+        this.isFaceUp = false;
+        this.isMatched = false;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public boolean isFaceUp() {
+        return isFaceUp;
+    }
+
+    public void flip() {
+        isFaceUp = !isFaceUp;
+    }
+
+    public boolean isMatched() {
+        return isMatched;
+    }
+
+    public void setMatched(boolean matched) {
+        isMatched = matched;
+    }
 }
